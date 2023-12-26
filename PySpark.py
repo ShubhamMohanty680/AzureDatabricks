84f3e8fcad77d31f7ecb00a4f79faed783729b2b
vishalsingh17 <vishalsingh17>
2023-12-26 03:56:42 +0000

    Updated PySpark.py

--- PySpark.py
+++ PySpark.py
@@ -1,2 +1,392 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC #### Reading file from storage
+
+# COMMAND ----------
+
+df = spark.read.format("delta") \
+    .option("inferSchema", "true") \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .load("dbfs:/user/hive/warehouse/airlines")
+
+display(df)
+
+# COMMAND ----------
+
+# check schema
+
+df.printSchema()
+
+# COMMAND ----------
+
+# create a view
+
+df.createOrReplaceTempView("airlines_temp_view")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from airlines_temp_view;
+
+# COMMAND ----------
+
+# presist the dataframe in permanent storage
+
+df.write.format("parquet").saveAsTable("airlines_parquet")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from airlines_parquet;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC #### Renaming the columns
+
+# COMMAND ----------
+
+# method 1 (using withColumnRenamed() )
+
+#renaming single column
+
+df1 = df.withColumnRenamed("Name", "Airline Name")
+df1.show()
+
+# COMMAND ----------
+
+# renaming multiple columns
+
+df2 = df.withColumnRenamed("Name", "Airline Name").withColumnRenamed("Active", "Status")
+
+df2.show()
+
+# COMMAND ----------
+
+# method 2 (using selectExpr() function)
+# selectExpr() select only the specified columns. Throws error while working with column names having space
+df3 = df.selectExpr("Name", "IATA")
+df3.show()
+
+# COMMAND ----------
+
+df3 = df.selectExpr("Name as Airlines Name", "IATA")
+df3.show()
+
+# COMMAND ----------
+
+df3 = df.selectExpr("Name as Airlines_Name", "IATA")
+df3.show()
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 
