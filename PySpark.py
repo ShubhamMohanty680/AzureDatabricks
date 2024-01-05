4066e63066fcc115aaeb7191aabd2d83eaecb3e3
vishalsingh17 <vishalsingh17>
2024-01-05 06:19:20 +0000

    Updated PySpark.py

--- PySpark.py
+++ PySpark.py
@@ -582,63 +582,83 @@ dbutils.fs.ls("/FileStore/tables/airlines1/Active=Y")
 
 # COMMAND ----------
 
+sampleData = [
+    ("Scott","Finance","NY",83000,36,19000),
+    ("Jen","Finance","NY",79000,53,15000),
+    ("Jeff","Marketing","CA",80000,25,18000),
+    ("Kumar","Marketing","NY",91000,50,21000),
+    ("James","Sales","NY",90000,34,10000),
+    ("Michael","Sales","NY",86000,56,20000),
+    ("Robert","Sales","CA",81000,30,23000),
+    ("Maria","Finance","CA",90000,24,23000),
+    ("Raman","Finance","CA",99000,40,24000)
+  ]
+
+schema = ["employee_name","department","state","salary","age","bonus"]
+emp_df = spark.createDataFrame(data=sampleData, schema = schema)
+
+# COMMAND ----------
+
+display(emp_df)
+
+# COMMAND ----------
+
+emp_df.write.option("header", "true").mode("overwrite").csv("/FileStore/tables/emp_data")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/FileStore/tables/emp_data")
+
+# COMMAND ----------
+
+emp_df.write.option("header", "true").partitionBy("state").mode("overwrite").csv("/FileStore/tables/emp_data")
 
 
 # COMMAND ----------
 
+dbutils.fs.ls("/FileStore/tables/emp_data")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/FileStore/tables/emp_data/state=CA")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/FileStore/tables/emp_data/state=NY")
+
+# COMMAND ----------
+
+# column / columns on which the partition is made is not save in the part files
+
+spark.read.option("header", "true").format("csv").load("/FileStore/tables/emp_data/state=CA").show()
 
 
 # COMMAND ----------
 
+# column / columns on which the partition is made is not save in the part files
+
+spark.read.option("header", "true").format("csv").load("/FileStore/tables/emp_data/state=NY").show()
 
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
+emp_df.write.option("header", "true").partitionBy("department", "state").mode("overwrite").csv("/FileStore/tables/emp_data")
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
+dbutils.fs.ls("/FileStore/tables/emp_data")
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
+dbutils.fs.ls("/FileStore/tables/emp_data/department=Finance/")
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
+dbutils.fs.ls("/FileStore/tables/emp_data/department=Marketing/")
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
+dbutils.fs.ls("/FileStore/tables/emp_data/department=Sales/")
 
 # COMMAND ----------
 
